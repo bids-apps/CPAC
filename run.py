3c45a548f83f1a4ac5f6c0bb28a34d54000c4098
Cameron Craddock <cameron.craddock@gmail.com>
2016-08-08 18:15:05 -0400

    run.py takes in parameters now, still doesn't really do anything...

--- run.py
+++ run.py
@@ -1,14 +1,16 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 import argparse
 import os
-import nibabel
-import numpy
-from glob import glob
+#import nibabel
+#import numpy
+#from glob import glob
 from subprocess import Popen, PIPE
 from shutil import rmtree
 import subprocess
 import yaml
 import CPAC.utils as cpac_utils
+
+print("hello!")
 
 
 def run(command, env={}):
@@ -51,67 +53,101 @@ parser.add_argument('--save_working_dir', action='store_true',
 # get the command line arguments
 args = parser.parse_args()
 
+print(args)
 # get and set configuration
-c = cpac_utils.Configuration(yaml.load(open(os.path.realpath(config), 'r')))
+c = cpac_utils.Configuration(yaml.load(open(\
+    os.path.realpath(args.pipeline_file), 'r')))
 
-print c
-exit(1)
+# set the parameters using the command line arguements
 
-subjects_to_analyze = []
-# only for a subset of subjects
-if args.participant_label:
-    subjects_to_analyze = args.participant_label
-# for all subjects
+# we will need to check that the directories exist, and
+# make them if they do not
+c.outputDirectory = os.path.join(args.output_dir, "output")
+c.workingDirectory = os.path.join(args.output_dir, "working")
+c.crashLogDirectory = os.path.join(args.output_dir, "crash")
+c.logDirectory = os.path.join(args.output_dir, "log")
+
+c.memoryAllocatedPerSubject = int(args.mem)
+c.numCoresPerSubject = int(args.n_cpus)
+c.numSubjectsAtOnce = 1
+c.num_ants_threads = min(args.n_cpus,4)
+if( args.save_working_dir == True ):
+    c.removeWorkingDir = False
 else:
-    subject_dirs = glob(os.path.join(args.bids_dir, "sub-*"))
-    subjects_to_analyze = \
-        [subject_dir.split("-")[-1] for subject_dir in subject_dirs]
+    c.removeWorkingDir = True
 
-# running participant level
-if args.analysis_level == "participant":
-    # find all T1s and skullstrip them
-    for subject_label in subjects_to_analyze:
-        # grab all T1s from all sessions
-        input_args = " ".join(["-i %s"%f for f in \
-            glob(os.path.join(args.bids_dir,"sub-%s"%subject_label,"anat",
-                "*_T1w.nii*")) + \
-            glob(os.path.join(args.bids_dir,"sub-%s"%subject_label,"ses-*",
-                "anat", "*_T1w.nii*"))])
-        cmd = "echo 'CPAC participant analysis: %s %s %s'"%(subject_label,
-            args.output_dir, input_args)
-        print(cmd)
-        if os.path.exists(os.path.join(args.output_dir, subject_label)):
-            rmtree(os.path.join(args.output_dir, subject_label))
-        run(cmd)
+print ("#### Running C-PAC on %s"%(args.participant_label))
+print ("Number of subjects to run in parallel: %d"%(c.numSubjectsAtOnce))
+print ("Output directory: %s"%(c.outputDirectory))
+print ("Working directory: %s"%(c.workingDirectory))
+print ("Crash directory: %s"%(c.crashLogDirectory))
+print ("Log directory: %s"%(c.logDirectory))
+print ("Remove working directory: %s"%(c.removeWorkingDir))
+print ("Available memory: %d (GB)"%(c.memoryAllocatedPerSubject))
+print ("Available threads: %d"%(c.numCoresPerSubject))
+print ("Number of threads for ANTs: %d"%(c.num_ants_threads))
 
-   # Import packages
-    import commands
-    commands.getoutput('source ~/.bashrc')
-    import yaml
-    
-    
-    # Try and load in the subject list
-    try:
-        sublist = yaml.load(open(os.path.realpath(subject_list_file), 'r'))
-    except:
-        raise Exception ("Subject list is not in proper YAML format. Please check your file")
-    
-    # Grab the subject of interest
-    sub_dict = sublist[int(indx)-1]
-    sub_id = sub_dict['subject_id']
-
-    try:
-        # Build and run the pipeline
-        prep_workflow(sub_dict, c, pickle.load(open(strategies, 'r')), 1, p_name, plugin=plugin, plugin_args=plugin_args)
-    except Exception as e:
-        print 'Could not complete cpac run for subject: %s!' % sub_id
-        print 'Error: %s' % e
+# read in the directory to find the input files
 
 
 
-elif args.analysis_level == "group":
-    # running group level
-    # generate study specific template
-    cmd = "echo 'CPAC group analysis " +  " ".join(subjects_to_analyze) + "'"
-    print(cmd)
-    run(cmd, env={"SUBJECTS_DIR": args.output_dir})
+#
+#
+#subjects_to_analyze = []
+## only for a subset of subjects
+#if args.participant_label:
+    #subjects_to_analyze = args.participant_label
+## for all subjects
+#else:
+    #subject_dirs = glob(os.path.join(args.bids_dir, "sub-*"))
+    #subjects_to_analyze = \
+        #[subject_dir.split("-")[-1] for subject_dir in subject_dirs]
+#
+## running participant level
+#if args.analysis_level == "participant":
+    ## find all T1s and skullstrip them
+    #for subject_label in subjects_to_analyze:
+        ## grab all T1s from all sessions
+        #input_args = " ".join(["-i %s"%f for f in \
+            #glob(os.path.join(args.bids_dir,"sub-%s"%subject_label,"anat",
+                #"*_T1w.nii*")) + \
+            #glob(os.path.join(args.bids_dir,"sub-%s"%subject_label,"ses-*",
+                #"anat", "*_T1w.nii*"))])
+        #cmd = "echo 'CPAC participant analysis: %s %s %s'"%(subject_label,
+            #args.output_dir, input_args)
+        #print(cmd)
+        #if os.path.exists(os.path.join(args.output_dir, subject_label)):
+            #rmtree(os.path.join(args.output_dir, subject_label))
+        #run(cmd)
+#
+   ## Import packages
+    #import commands
+    #commands.getoutput('source ~/.bashrc')
+    #import yaml
+    #
+    #
+    ## Try and load in the subject list
+    #try:
+        #sublist = yaml.load(open(os.path.realpath(subject_list_file), 'r'))
+    #except:
+        #raise Exception ("Subject list is not in proper YAML format. Please check your file")
+    #
+    ## Grab the subject of interest
+    #sub_dict = sublist[int(indx)-1]
+    #sub_id = sub_dict['subject_id']
+#
+    #try:
+        ## Build and run the pipeline
+        #prep_workflow(sub_dict, c, pickle.load(open(strategies, 'r')), 1, p_name, plugin=plugin, plugin_args=plugin_args)
+    #except Exception as e:
+        #print 'Could not complete cpac run for subject: %s!' % sub_id
+        #print 'Error: %s' % e
+#
+#
+#
+#elif args.analysis_level == "group":
+    ## running group level
+    ## generate study specific template
+    #cmd = "echo 'CPAC group analysis " +  " ".join(subjects_to_analyze) + "'"
+    #print(cmd)
+    #run(cmd, env={"SUBJECTS_DIR": args.output_dir})
