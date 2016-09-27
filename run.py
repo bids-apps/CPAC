82ae64563ad613b68ee899724e843e2d91ed1a54
carolFrohlich <carol.frohlich@gmail.com>
2016-09-27 17:58:17 -0400

    create bids sublist

--- run.py
+++ run.py
@@ -3,14 +3,14 @@ import argparse
 import os
 #import nibabel
 #import numpy
-#from glob import glob
+from glob import glob
 from subprocess import Popen, PIPE
 from shutil import rmtree
 import subprocess
 import yaml
 import CPAC.utils as cpac_utils
 
-print("hello!")
+
 
 
 def run(command, env={}):
@@ -28,7 +28,7 @@ parser.add_argument('bids_dir', help='The directory with the input dataset '
     'formatted according to the BIDS standard.')
 parser.add_argument('output_dir', help='The directory where the output files '
     'should be stored. If you are running group level analysis '
-    'this folder should be prepopulated with the results of the'
+    'this folder should be prepopulated with the results of the '
     'participant level analysis.')
 parser.add_argument('analysis_level', help='Level of the analysis that will '
     ' be performed. Multiple participant level analyses can be run '
@@ -92,36 +92,69 @@ print ("Number of threads for ANTs: %d"%(c.num_ants_threads))
 
 # read in the directory to find the input files
 
+subjects_to_analyze = []
+# only for a subset of subjects
+if args.participant_label:
+    subjects_to_analyze = args.participant_label
+
+# for all subjects
+else:
+    subject_dirs = glob(os.path.join(args.bids_dir, "sub-*"))
+    subjects_to_analyze = \
+        [subject_dir.split("-")[-1] for subject_dir in subject_dirs]
 
 
-#
-#
-#subjects_to_analyze = []
-## only for a subset of subjects
-#if args.participant_label:
-    #subjects_to_analyze = args.participant_label
-## for all subjects
-#else:
-    #subject_dirs = glob(os.path.join(args.bids_dir, "sub-*"))
-    #subjects_to_analyze = \
-        #[subject_dir.split("-")[-1] for subject_dir in subject_dirs]
-#
-## running participant level
-#if args.analysis_level == "participant":
-    ## find all T1s and skullstrip them
-    #for subject_label in subjects_to_analyze:
-        ## grab all T1s from all sessions
-        #input_args = " ".join(["-i %s"%f for f in \
-            #glob(os.path.join(args.bids_dir,"sub-%s"%subject_label,"anat",
-                #"*_T1w.nii*")) + \
-            #glob(os.path.join(args.bids_dir,"sub-%s"%subject_label,"ses-*",
-                #"anat", "*_T1w.nii*"))])
-        #cmd = "echo 'CPAC participant analysis: %s %s %s'"%(subject_label,
-            #args.output_dir, input_args)
-        #print(cmd)
-        #if os.path.exists(os.path.join(args.output_dir, subject_label)):
-            #rmtree(os.path.join(args.output_dir, subject_label))
-        #run(cmd)
+#create subject list
+if args.analysis_level == "participant":
+    sublist = []
+    for subject_label in subjects_to_analyze:
+        anat = " ".join(["%s"%f for f in \
+            glob(os.path.join(args.bids_dir,"sub-%s"%subject_label,"anat",
+                "*_T1w.nii*")) + \
+            glob(os.path.join(args.bids_dir,"sub-%s"%subject_label,"ses-*",
+                "anat", "*_T1w.nii*"))])
+
+        #func
+        func_files = \
+            glob(os.path.join(args.bids_dir,"sub-%s"%subject_label,"func",
+                "*_bold.nii*")) + \
+            glob(os.path.join(args.bids_dir,"sub-%s"%subject_label,"ses-*",
+                "func", "*_bold.nii*"))
+        func = {}
+        for f in func_files:
+            func_id = f.split('-')[-1].split('_')[0]
+            func[func_id] = f
+
+        new_sub = {}
+        new_sub['subject_id'] = 'sub-%s'%(subject_label)
+        ##??new_sub['unique_id'] = 'sub-'%(subject_label)
+        new_sub['anat'] = anat
+        new_sub['rest'] = func
+        sublist.append(new_sub)
+    with open('subject_list.yaml', 'w') as f:
+        yaml.dump(sublist, f)
+
+
+#TOD: Build and run the pipeline
+
+
+########  CC code #############
+# running participant level
+if args.analysis_level == "participant":
+    # find all T1s and skullstrip them
+    for subject_label in subjects_to_analyze:
+        # grab all T1s from all sessions
+        input_args = " ".join(["-i %s"%f for f in \
+            glob(os.path.join(args.bids_dir,"sub-%s"%subject_label,"anat",
+                "*_T1w.nii*")) + \
+            glob(os.path.join(args.bids_dir,"sub-%s"%subject_label,"ses-*",
+                "anat", "*_T1w.nii*"))])
+        cmd = "echo 'CPAC participant analysis: %s %s %s'"%(subject_label,
+            args.output_dir, input_args)
+        print(cmd)
+        if os.path.exists(os.path.join(args.output_dir, subject_label)):
+            rmtree(os.path.join(args.output_dir, subject_label))
+        run(cmd)
 #
    ## Import packages
     #import commands
