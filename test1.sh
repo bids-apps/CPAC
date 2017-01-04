2e4884145f12a2d628a0fc3ac3e23a321ecfedec
Cameron Craddock <cameron.craddock@gmail.com>
2017-01-04 01:29:43 -0500

    removed singularity deply from circle.yml, does not work since circeci containers are not run in privileged mode

--- test1.sh
+++ test1.sh
@@ -12,3 +12,14 @@ docker run -ti --rm --read-only \
     --participant_label 01 \
     --n_cpus 2 \
     --pipeline_file /cpac_resources/test_pipeline.yaml
+
+mkdir -p ${BUILDIR}/singularity_images
+docker run --privileged -ti --rm \
+    -v /var/run/docker.sock:/var/run/docker.sock \
+    -v ${HOME}/singularity_images:/output \
+    filo/docker2singularity "bids/cpac:latest"
+
+for i in ${HOME}/singularity_images/*.img;
+do
+
+done
