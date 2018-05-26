997e6747ec8fdf96efa493022125f25ad8af7a25
Cameron Craddock <cameron.craddock@gmail.com>
2018-05-25 22:46:52 -0400

    debug creds file

--- test_docker.sh
+++ test_docker.sh
@@ -7,13 +7,15 @@ docker run -it --rm \
      cpac_next \
      s3://fcp-indi/data/Projects/ABIDE/RawDataBIDS/Caltech \
      s3://dms-iaic/cpac_test/test1/output \
-     participant \
+     test_config \
      --n_cpus 8 \
      --mem_gb 16 \
      --pipeline_file /mnt/test_pipeline.yaml \
-     --aws_output_creds /mnt/s3_cpac_test.csv \
+     --aws_output_creds env \
      --participant_label '0051480 0051481' \
      --participant_ndx 1 
+
+
      #--data_config_file s3://fcp-indi/data/Projects/ABIDE/cpac_data_config.yml \
 #docker run -it --rm \
      #-v /home/ccraddock/CPAC/outputs:/mnt \
