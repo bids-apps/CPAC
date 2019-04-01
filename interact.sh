bccb4a58fa48d8a08d03329b278429d496618cdc
anibalsolon <anibalsolon@gmail.com>
2019-04-01 10:43:03 -0400

    dummy repo

--- interact.sh
+++
@@ -1,9 +0,0 @@
-BUILDIR=$(pwd)
-
-docker run -ti --rm --read-only \
-    -v /tmp:/tmp \
-    -v /var/tmp:/var/tmp \
-    -v ${BUILDIR}/data/ds114_test1:/bids_dataset \
-    -v ${BUILDIR}/outputs:/outputs \
-    --entrypoint /bin/bash \
-    bids/cpac 
--- make_singularity.sh
+++
@@ -1,6 +0,0 @@
-docker run --privileged -ti --rm  \
-    -v /var/run/docker.sock:/var/run/docker.sock \
-    -v ${PWD}/singularity_images:/output \
-    filo/docker2singularity \
-    bids/cpac
-  
